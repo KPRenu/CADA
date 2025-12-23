e1e97af9d7b8c14e38c9759cff326de0626682a2
KPRenu <130354166+KPRenu@users.noreply.github.com>
2025-12-23 19:13:11 +0530

    Remove comments from train function
    Removed comments from the train function to streamline code.

--- ml1.py
+++ ml1.py
@@ -7,13 +7,11 @@ concepts = np.array(data)[:, :-1]
 target = np.array(data)[:, -1]
 
 def train(con, tar):
-    # Step 1: Find the first positive example
     for i, val in enumerate(tar):
         if val == 'yes':
             specific_h = con[i].copy()
             break
 
-    # Step 2: Update specific hypothesis for all positive examples
     for i, val in enumerate(con):
         if tar[i] == 'yes':
             for x in range(len(specific_h)):
